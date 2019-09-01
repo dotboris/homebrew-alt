52b46be64e4d2cc0a5d72f5fec153e021cf42900
Boris Bera <bera.boris@gmail.com>
2019-09-01 18:08:00 -0400

    Add basic formula for installing alt

---
+++ Formula/alt.rb
@@ -0,0 +1,28 @@
+class Alt < Formula
+  desc 'A simple version manager. Tool for switching between different versions of commands'
+  homepage 'https://github.com/dotboris/alt'
+  url 'https://github.com/dotboris/alt/archive/v1.0.5.tar.gz'
+  sha256 '90986955183248882584ccfe4e558ca5f610c4bcc8da8d5fe13352522f8226c5'
+
+  # depends_on "cmake" => :build
+  depnds_on 'rust' => :build
+
+  def install
+   system 'cargo', 'install', '--release', '--locked'
+          '--root', prefix,
+          '--path', '.'
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test alt`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
