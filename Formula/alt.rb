3d2cb04505606f24337a81eb5f62662955ddd77c
Boris Bera <bbera@pmctire.com>
2019-11-16 14:27:44 -0500

    Update source package to v1.1.1

--- Formula/alt.rb
+++ Formula/alt.rb
@@ -1,8 +1,8 @@
 class Alt < Formula
   desc "Tool for switching between different versions of commands"
   homepage "https://github.com/dotboris/alt"
-  url "https://github.com/dotboris/alt/archive/v1.1.0.tar.gz"
-  sha256 "46556210433155b274ea89192797eca8e0f89032b9b848bf14cf76bf51047664"
+  url "https://github.com/dotboris/alt/archive/v1.1.1.tar.gz"
+  sha256 "6b3f12f0b24a81ac7948d73e69b817fc1d45fd848d7f422032f006c93c7b9030"
   head "https://github.com/dotboris/alt.git"
 
   depends_on "rust" => :build
